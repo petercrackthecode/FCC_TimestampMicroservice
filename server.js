5ee31999aee9b555685dec66790da00687f09e74
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-03-28 13:31:28 -0700

    added a bunch of stuff

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-const fs= require('fs');
+const express = require("express");
+const app = express();
+require('dotenv').config();
 
-fs.writeFile('./static/index.html', '', 'utf-8', (err) => {
-    if (err) console.log(err);
-    else console.log('index.html successfully created');
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+
+let cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/static/index.html");
 });
+
+// your first API endpoint...
+app.get("/api/hello", (req, res) => {
+  res.json({ greeting: "Hello API" });
+});
+
+app.listen(process.env.PORT);
+
